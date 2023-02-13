026162105a71da30ccabb719c8c0a4c15a358f4a
EricFormar <menaeric@hotmail.com>
2023-02-13 12:08:25 -0300

    refactor view engine setup

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const coursesRouter = require('./routes/courses')
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
 
 app.use(logger('dev'));
 app.use(express.json());
